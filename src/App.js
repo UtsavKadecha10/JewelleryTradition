6eb3378ccf49c51abe41147a61cd599ec46d561e
Utsav <ut.kadecha@gmail.com>
2024-05-02 12:33:24 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import Layout from './Components/Layout';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Layout/>} />
+          {/* <Route path='/login' element={<Login />} />
+          <Route path='/logout' element={<Logout />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='*' element={<Nopage />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </>
+
   );
 }
 
---
+++ src/Components/Layout.js
@@ -0,0 +1,256 @@
+import { Outlet, Link } from "react-router-dom";
+import { useState } from "react";
+import './Layout.css';
+import account from './images/account.png';
+import wishlist from './images/heart.png'
+import cart from './images/cart.png'
+import menu from './images/menu.png'
+import { Dropdown } from "react-bootstrap/Dropdown";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Carousel } from 'react-bootstrap';
+import sample from './images/sa.webp'
+import Slider from './images/ExampleCarouselImage.jpg'
+import store from './images/store.png'
+
+
+
+const Layout=()=>{
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex) => {
+        setIndex(selectedIndex);
+    };
+    return(
+        <>
+            <header>
+                <div className='navTotal'>
+                  <nav className="nav1">
+                      <div className="menu-div">
+                          <button className='menu-btn' id='move-header'><img id='move-header' src={menu}/></button>
+                          <div className='sidebar-menu' id='sidebar-menu'>
+                              <ul className='sidebar-jewellery-list'>
+                                  <li>Jadau Jewellery</li>
+                                  <li>Silver Jewellery</li>
+                                  <li>Diamond Jewellery</li>
+                                  <li>Antiques</li>
+                              </ul>
+                              <ul className='sidebar-footer-list'>
+                                  <li>Log In</li>
+                                  <li>Create Account</li>
+                                  <li>About Us</li>
+                                  <li>Contact Us</li>
+                              </ul>
+                          </div>
+                      </div>
+                      <div>
+                          <h3 href="#home" id='move-header' className='title'>JEWELLERY TRADITION</h3>
+                      </div>
+                      <div id='move-header'>
+                          <ul className="iconList">
+                              <li className="wishIcon">
+                                  <a href=""><img src={wishlist}/></a>
+                              </li>
+                              <li className="wishIcon">
+                                  <a href=""><img src={store}/></a>
+                              </li>
+                          </ul>
+                      </div>
+                  </nav>
+                  <div className='dropdown-outer' id='move-header'>
+                      <div className="dropdown">
+                          <button className="dropbtn">ALL JEWELLERY</button>
+                          <ul className="dropdown-content">
+                              <li><a href="#">Jadau</a></li>
+                              <li><a href="#">Diamond</a></li>
+                              <li><a href="#">Silver</a></li>
+                          </ul>
+                      </div>
+                      <div className="dropdown">
+                          <button className="dropbtn">GOLD</button>
+                          <ul className="dropdown-content">
+                              <li><a href="#">Jadau</a></li>
+                              <li><a href="#">Diamond</a></li>
+                              <li><a href="#">Silver</a></li>
+                          </ul>
+                      </div>
+                      <div className="dropdown">
+                          <button className="dropbtn">DIAMOND</button>
+                          <ul className="dropdown-content">
+                              <li><a href="#">Jadau</a></li>
+                              <li><a href="#">Diamond</a></li>
+                              <li><a href="#">Silver</a></li>
+                          </ul>
+                      </div>
+                      <div className="dropdown">
+                          <button className="dropbtn">SILVER</button>
+                          <ul className="dropdown-content">
+                              <li><a href="#">Jadau</a></li>
+                              <li><a href="#">Diamond</a></li>
+                              <li><a href="#">Silver</a></li>
+                          </ul>
+                      </div>
+                      <div className="dropdown">
+                          <button className="dropbtn">ANTIQUE</button>
+                          <ul className="dropdown-content">
+                              <li><a href="#">Jadau</a></li>
+                              <li><a href="#">Diamond</a></li>
+                              <li><a href="#">Silver</a></li>
+                          </ul>
+                      </div>
+                      <div className="dropdown">
+                          <button className="dropbtn">MORE</button>
+                          <ul className="dropdown-content">
+                              <li><a href="#">Jadau</a></li>
+                              <li><a href="#">Diamond</a></li>
+                              <li><a href="#">Silver</a></li>
+                          </ul>
+                      </div>
+                  </div>
+                </div>
+                
+            </header>
+            <main>
+            <Carousel>
+                  <Carousel.Item className="slider-image">
+                    <img
+                      className="d-block w-100"
+                      src={Slider}
+                      alt="First slide"
+                    />
+                    <Carousel.Caption>
+                      
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                  <Carousel.Item className="slider-image"> 
+                    <img
+                      className="d-block w-100"
+                      src={Slider}
+                      alt="Second slide"
+                    />
+                    <Carousel.Caption >
+                     
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                  <Carousel.Item className="slider-image">
+                    <img
+                      className="d-block w-100"
+                      src={Slider}
+                      alt="Third slide"
+                    />
+                    <Carousel.Caption>
+                      
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                </Carousel>
+                <section className="orderButton" id='move-header'>
+                        {/* <h2>Stunning Designs curated only for you!</h2> */}
+                        <a href="#"><button>SHOP NOW</button></a>
+                </section>
+                <div className='new-for-you section'>
+                    <div className="main_head">
+                        <h1>New For You!</h1>
+                        <h4>Our latest releases, just for you!</h4>
+                    </div>
+                    <div className="jewellery_card">
+                        <ul>
+                            <li className="j1 card">
+                                <a href="" className="images">
+                                    <img src={sample}/>
+                                    <span>Explore More</span>
+                                </a>
+                            </li>
+                            <li className="j2 card">
+                                <a href="" className="images">
+                                    <img src={sample}/>
+                                    <span>Explore More</span>
+                                </a>
+                            </li>
+                            <li className="j3 card hide-on-small">
+                                <a href="" className="images">
+                                    <img src={sample}/>
+                                    <span>Explore More</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <hr></hr>
+                <div className='shop-by-category section'>
+                    <div className="main_head">
+                        <h1>Shop By Category</h1>
+                        <h4>Browse through your favorite categories. We've got them all!</h4>
+                    </div>
+                    <div className="jewellery_card">
+                        <ul>
+                            <li className="j1 card">
+                                <a href="" className="images">
+                                    <img src={sample}/>
+                                    <span>Explore More</span>
+                                </a>
+                            </li>
+                            <li className="j2 card">
+                                <a href="" className="images">
+                                    <img src={sample}/>
+                                    <span>Explore More</span>
+                                </a>
+                            </li>
+                            <li className="j3 card hide-on-small">
+                                <a href="" className="images">
+                                    <img src={sample}/>
+                                    <span>Explore More</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <hr></hr>
+                <div className='connect section'>
+                    <div className="main_head">
+                        <h1>Connect with Us!</h1>
+                        <h4>We are always available to guide you at your convenience.</h4>
+                    </div>
+                    <div className="jewellery_card">
+                        <ul>
+                            <li className="j1 card">
+                                <a href="" className="images">
+                                    <img src={sample}/>
+                                    <span>Explore More</span>
+                                </a>
+                            </li>
+                            <li className="j2 card">
+                                <a href="" className="images">
+                                    <img src={sample}/>
+                                    <span>Explore More</span>
+                                </a>
+                            </li>
+                            <li className="j3 card hide-on-small">
+                                <a href="" className="images ">
+                                    <img src={sample}/>
+                                    <span>Explore More</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </main>
+            <footer>
+                <div>
+                    <h3>About Us</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur fhf fuga fugiat ab quos praesentium optio quasi laudantium.</p>
+                </div>
+                <div>
+                    <h3>Contact Us</h3>
+                    <ul className='footer-list'>
+                        <li><i className="fa fa-map-marker"></i>Lal Katra, Haldiyon ka Rasta, Johri Bazar, Jaipur</li>
+                        <li><i className="fa fa-phone"></i>+91 82390 73110</li>
+                        <li><i className="fa fa-envelope"></i><a href="mailto:info@example.com">info@example.com</a></li>
+                    </ul>
+                </div>
+            </footer>
+        </>
+    );
+}
+export default Layout;
